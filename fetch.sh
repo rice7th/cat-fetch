b2530c81ce044dc9d0764f50ba7a9c977f57c2bb
JhonnyRice <93940240+JhonnyRice@users.noreply.github.com>
2022-01-18 22:08:43 +0100

    arch fixed

--- fetch.sh
+++ fetch.sh
@@ -3,7 +3,7 @@
 
 OS=$(cat /proc/version | grep -o -m 1 "ubuntu\|arch\|fedora\|debian\|manjaro\|alpine\|soda\|instant\|pop_os\|redhat\|red hat\|centos\|parabola\|arco\|opensuse\|suse\|void\|steam\|mx\|gentoo\|calculate")
 WM=$(id=$(xprop -root -notype | awk '$1=="_NET_SUPPORTING_WM_CHECK:"{print $5}'); xprop -id "${id}" -notype -f _NET_WM_NAME 8t | grep "_NET_WM_NAME = " | cut --delimiter=' ' --fields=3 | cut --delimiter='"' --fields=2)
-ARCH=$(uname -i)
+ARCH=$(uname -m)
 NODE=$(uname -n)
 KERNEL_NAME=$(uname -s)
 KERNEL_REL=$(uname -r)
