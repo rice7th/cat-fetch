cb2972c68529c6a9f96a9a6d9ac4029c9f4c23c8
JhonnyRice <93940240+JhonnyRice@users.noreply.github.com>
2022-01-18 21:55:45 +0100

    added stuff :)

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 #Wm script by Alberto Salvia Novella (es20490446e.wordpress.com)
+
 OS=$(cat /proc/version | grep -o -m 1 "ubuntu\|arch\|fedora\|debian\|manjaro\|alpine\|soda\|instant\|pop_os\|redhat\|red hat\|centos\|parabola\|arco\|opensuse\|suse\|void\|steam\|mx\|gentoo\|calculate")
 WM=$(id=$(xprop -root -notype | awk '$1=="_NET_SUPPORTING_WM_CHECK:"{print $5}'); xprop -id "${id}" -notype -f _NET_WM_NAME 8t | grep "_NET_WM_NAME = " | cut --delimiter=' ' --fields=3 | cut --delimiter='"' --fields=2)
-echo "os:${OS}, wm:${WM}"
+ARCH=$(uname -i)
+NODE=$(uname -n)
+KERNEL_NAME=$(uname -s)
+KERNEL_REL=$(uname -r)
+SEPARATOR="-"
+echo "${NODE}"
+for i in {1..20}
+do
+    printf "${SEPARATOR}"
+done
+printf "\n"
+
+echo "   /'._        os:     ${OS}"
+echo "  (° o 7       wm:     ${WM}"
+echo "  |'-'\"~.  .   arch:   ${ARCH}"
+echo "  Uu^~C_J._.\"  kernel: ${KERNEL_NAME} ${KERNEL_REL}"
