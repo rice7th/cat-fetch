c2207cd08f32ed4b980998f1c29e9d3151ad6708
franci <franci@VoidBox.com>
2022-01-18 20:11:42 +0100

    Corrected misspelled Distros

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #Wm script by Alberto Salvia Novella (es20490446e.wordpress.com)
-OS=$(cat /proc/version | grep -o -m 1 "ubuntu\|arch\|fedora\|debian\|majaro\|alpine\|soda\|instant\|pop_os!\|redhat\|red hat\|centos\|parabola\|arco\|opensuse\|suse\|void\|steam\|mx\|gentoo\|calculate")
+OS=$(cat /proc/version | grep -o -m 1 "ubuntu\|arch\|fedora\|debian\|manjaro\|alpine\|soda\|instant\|pop_os\|redhat\|red hat\|centos\|parabola\|arco\|opensuse\|suse\|void\|steam\|mx\|gentoo\|calculate")
 WM=$(id=$(xprop -root -notype | awk '$1=="_NET_SUPPORTING_WM_CHECK:"{print $5}'); xprop -id "${id}" -notype -f _NET_WM_NAME 8t | grep "_NET_WM_NAME = " | cut --delimiter=' ' --fields=3 | cut --delimiter='"' --fields=2)
 echo "os:${OS}, wm:${WM}"
