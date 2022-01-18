13577c33a7fd888edbba8ee319b17a2c0f64e11e
JhonnyRice <93940240+JhonnyRice@users.noreply.github.com>
2022-01-18 19:29:11 +0100

    pop os added

--- getos.sh
+++ getos.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /proc/version | grep -o -m 1 "ubuntu\|arch\|fedora\|debian\|majaro\|alpine\|soda\|instant"
+cat /proc/version | grep -o -m 1 "ubuntu\|arch\|fedora\|debian\|majaro\|alpine\|soda\|instant\|pop_os!"
