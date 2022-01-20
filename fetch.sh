da16be1f22e91c2fe4480d1b01551f007e110bca
JhonnyRice <93940240+JhonnyRice@users.noreply.github.com>
2022-01-20 15:49:05 +0100

    ae

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
+# CUSTOMIZZATION ZONE
+
+
+
+
+# Cool characters for the symbol ◆ ◀ ▧ ▨ ▦ ▥ ▤ ▣ ● ◕ █
+SYMBOL="●"
+SEPARATOR="-"
+
+
+
+
+
+
+
+
+
+#COLOR VARIABLES
+RED="\x1b[31m"
+YELLOW="\x1b[33m"
+GREEN="\x1b[32m"
+CYAN="\x1b[36m"
+BLUE="\x1b[34m"
+PURPLE="\x1b[35m"
+BLACK="\x1b[30m"
+WHITE="\x1b[37m"
+B_RED="\x1b[91m"
+B_YELLOW="\x1b[93m"
+B_GREEN="\x1b[92m"
+B_CYAN="\x1b[96m"
+B_BLUE="\x1b[94m"
+B_PURPLE="\x1b[95m"
+B_WHITE="\x1b[97m"
+GRAY="\x1b[90m"
+CLS="\x1b[0m"
+
 #Wm script by Alberto Salvia Novella (es20490446e.wordpress.com)
 OS=$(cat /proc/version | grep -o -m 1 "ubuntu\|arch\|fedora\|debian\|manjaro\|alpine\|soda\|instant\|pop_os\|redhat\|red hat\|centos\|parabola\|arco\|opensuse\|suse\|void\|steam\|mx\|gentoo\|calculate" | uniq)
 WM=$(id=$(xprop -root -notype | awk '$1=="_NET_SUPPORTING_WM_CHECK:"{print $5}'); xprop -id "${id}" -notype -f _NET_WM_NAME 8t | grep "_NET_WM_NAME = " | cut --delimiter=' ' --fields=3 | cut --delimiter='"' --fields=2)
@@ -6,35 +42,47 @@ ARCH=$(uname -m)
 NODE=$(uname -n)
 KERNEL_NAME=$(uname -s)
 KERNEL_REL=$(uname -r)
-
+SHELL_NAME=$(echo $SHELL | sed 's/bin//g')
 #source: https://unix.stackexchange.com/a/681480
 SESSION=${DISPLAY:+X11}${WAYLAND_DISPLAY:+WAYLAND}
-UPTIME=$(uptime -p | sed s/up// | sed 's/,//g')
+UPTIME=$(uptime -p | sed s/up// | sed 's/,//g'| sed 's/ //')
 
-# Cool characters for the symbol ◆ ◀ ▧ ▨ ▦ ▥ ▤ ▣ ● ◕ █
-SYMBOL="●"
-SEPARATOR="-"
+
+TXTCOL=$YELLOW
+WORDCOL=$GREEN
+CATCOL=$CYAN
+
 
 ExtraInfo()
 {
-	echo "                arch: ${ARCH}"
-	echo "                graphic session: ${SESSION}"
+	echo -e "                ${WORDCOL}arch:    ${TXTCOL}${ARCH}"
+	echo -e "                ${WORDCOL}graphic session: ${TXTCOL}${SESSION}"
 }
 
-printf "$NODE\n"
+printf "                $NODE\n                "
 
 for (( i=0; i<${#NODE}; i++ )); do printf "-"; done
 printf "\e[0m\n"
 
-echo "   /'._         os: ${OS}"
-echo "  (° o 7        wm: ${WM}"
-echo "   |'-'\"~.  .   shell: $SHELL"
-echo "   Uu^~C_J._.\"  uptime: ${UPTIME}"
-echo "                kernel: ${KERNEL_NAME} ${KERNEL_REL}"
+echo -e "${CATCOL}   /'._         ${WORDCOL}os:      ${TXTCOL}${OS}"
+echo -e "${CATCOL}  (° o 7        ${WORDCOL}wm:      ${TXTCOL}${WM}"
+echo -e "${CATCOL}   |'-'\"~.  .   ${WORDCOL}shell:   ${TXTCOL}${SHELL_NAME:2}"
+echo -e "${CATCOL}   Uu^~C_J._.\"  ${WORDCOL}uptime:  ${TXTCOL}${UPTIME}"
+echo -e "${CATCOL}                ${WORDCOL}kernel:  ${TXTCOL}${KERNEL_NAME} ${KERNEL_REL}"
 while getopts ":m" option; do
    case $option in
       m) # 	Get More Info
          ExtraInfo
    esac
 done
-echo -e " \x1b[31m ${SYMBOL} \x1b[33m ${SYMBOL} \x1b[32m ${SYMBOL} \x1b[36m ${SYMBOL} \x1b[34m ${SYMBOL} \x1b[35m ${SYMBOL} \x1b[37m ${SYMBOL} \x1b[90m ${SYMBOL}\x1b[0m"
+
+printf "                ${CLS}"
+for (( i=0; i<24; i++ )); do printf "-"; done
+printf "\e[0m\n"
+echo -e "                ${RED} ${SYMBOL} ${YELLOW} ${SYMBOL} ${GREEN} ${SYMBOL} ${CYAN} ${SYMBOL} ${BLUE} ${SYMBOL} ${PURPLE} ${SYMBOL} ${WHITE} ${SYMBOL} ${BLACK} ${SYMBOL} ${CLS}"
+echo -e "                ${B_RED} ${SYMBOL} ${B_YELLOW} ${SYMBOL} ${B_GREEN} ${SYMBOL} ${B_CYAN} ${SYMBOL} ${B_BLUE} ${SYMBOL} ${B_PURPLE} ${SYMBOL} ${B_WHITE} ${SYMBOL} ${GRAY} ${SYMBOL} ${CLS}"
+
+# /'._   ._
+#(- = 7___.~'
+# |--''   )
+# LL_)'CC_J
