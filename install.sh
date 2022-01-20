5f08946af193ad9b5de4a0bee86c20915f924fe6
JhonnyRice <93940240+JhonnyRice@users.noreply.github.com>
2022-01-20 23:33:19 +0100

    fix

--- install.sh
+++ install.sh
@@ -19,12 +19,12 @@ str1="2"
 
 if [ ${choose} == "$str1" ]; then
   sudo wget --quiet https://raw.githubusercontent.com/SpyLima/cat-fetch-reborn/main/catfetch -P /usr/bin/
-  sudo chmod +x /usr/bin/catfetch.sh
+  sudo chmod +x /usr/bin/catfetch
 else
   if [ ! -d "$HOME/.local/bin/" ]; then 
     echo "Directory $HOME/.local/bin/ does not exist, creating"
     mkdir $HOME/.local/bin/
   fi
   wget --quiet https://raw.githubusercontent.com/SpyLima/cat-fetch-reborn/main/catfetch -P $HOME/.local/bin/
-  chmod +x $HOME/.local/bin/catfetch.sh
+  chmod +x $HOME/.local/bin/catfetch
 fi
