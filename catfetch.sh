519d7748c24c0397a0448069af3e167cc8cabc04
JhonnyRice <93940240+JhonnyRice@users.noreply.github.com>
2022-01-23 16:36:15 +0100

    done?

--- catfetch.sh
+++ catfetch.sh
@@ -34,7 +34,7 @@ CATCOL=$CYAN
 #Wm script by Alberto Salvia Novella (es20490446e.wordpress.com)
 CPU=$(grep -m 1 'model name' /proc/cpuinfo)
 RAM=$(free -h | grep "^Mem:" | awk '{print $3 "/" $2}')
-OS=$(cat /proc/version | grep -o -m 1 "ubuntu\|arch\|fedora\|debian\|manjaro\|alpine\|soda\|instant\|pop_os\|redhat\|red hat\|centos\|parabola\|arco\|opensuse\|suse\|void\|steam\|mx\|gentoo\|calculate" | uniq)
+OS=$(cat /etc/os-release | grep -m1 "NAME=" | cut -d '"' -f 2)
 WM=$(id=$(xprop -root -notype | awk '$1=="_NET_SUPPORTING_WM_CHECK:"{print $5}'); xprop -id "${id}" -notype -f _NET_WM_NAME 8t | grep "_NET_WM_NAME = " | cut --delimiter=' ' --fields=3 | cut --delimiter='"' --fields=2)
 ARCH=$(uname -m)
 NODE=$(uname -n)
